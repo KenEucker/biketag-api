616868c2fb711f772f91e72cd539a223ca2b2721
Septimiu Bloj <septimiubloj@gmail.com>
2021-04-16 15:41:07 +0300

    feat(gettag): wrap tag data and return as BikeTagApiResponse

--- src/sanity/getTag.ts
+++ src/sanity/getTag.ts
@@ -1,53 +1,62 @@
-import { SanityClient } from '@sanity/client';
-import { BikeTagApiResponse, TagData } from '../common/types';
+import { SanityClient } from '@sanity/client'
+import { BikeTagApiResponse, TagData } from '../common/types'
 
 export async function getTag(
   client: SanityClient,
   options: any
-  ): Promise<BikeTagApiResponse<TagData>> {
+): Promise<BikeTagApiResponse<TagData>> {
+  if (!options) {
+    throw new Error('no options')
+  }
 
-    if (!options) {
-      throw new Error('no options')
+  options = typeof options === 'string' ? { slug: options } : options
+  options = typeof options === 'number' ? { slug: `portland-tag-${options}` } : options
+  options.slug = options.slug ? options.slug : `portland-tag-${options.tagnumber}`
+
+  if (!options.slug.length) {
+    throw new Error('no slug')
+  }
+
+  let fields = ''
+
+  if (options.fields?.length) {
+    fields = `{${options.fields.join(',')} }`
+  }
+
+  const query = `*[_type == "tag" && slug.current == "${options.slug}" ][0]${fields}`
+  const params = {}
+
+  return client.fetch(query, params).then((tag) => {
+    // construct tagData object from tag
+    const tagData = options.fields?.length
+      ? options.fields.reduce((o: any, f: any) => {
+          o[f] = tag[f]
+          return o
+        }, {})
+      : {
+          slug: tag.slug,
+          tagnumber: tag.tagnumber,
+          mysteryImage: tag.mysteryImage,
+          mysteryImageUrl: tag.mysteryImageUrl,
+          game: tag.game,
+          player: tag.player,
+          hint: tag.hint,
+          discussionUrl: tag.discussionUrl,
+          foundLocation: tag.foundLocation,
+          gps: tag.gps,
+          foundImage: tag.foundImage,
+          foundImageUrl: tag.foundImageUrl,
+        }
+
+    // wrap tag in BikeTagApiResponse
+    const response = {
+      data: tagData,
+      status: 1,
+      success: true,
+      source: 'sanity',
     }
 
-    options = typeof options === "string" ? { slug: options } : options
-    options = typeof options === "number" ? { slug: `portland-tag-${options}` } : options
-    options.slug = options.slug ? options.slug : `portland-tag-${options.tagnumber}`
-    
-    if (!options.slug.length) {
-      throw new Error('no slug')
-    }
-
-    let fields = ''
-
-    if (options.fields?.length) {
-      fields = `{${options.fields.join(',')} }`
-    }
-
-    const query = `*[_type == "tag" && slug.current == "${options.slug}" ][0]${fields}`
-    const params = {}
-    return client.fetch(query, params).then((tag) => {
-
-      // construct tagData object from tag
-      const tagData = {
-        tagnumber: tag.tagnumber,
-        mysteryImage: tag.mysteryImage,
-        mysteryImageUrl: tag.mysteryImageUrl,
-        game: tag.game,
-        player: tag.player,
-        hint: tag.hint,
-        discussionUrl: tag.discussionUrl,
-        foundLocation: tag.foundLocation,
-        gps: tag.gps,
-        foundImage: tag.foundImage,
-        foundImageUrl: tag.foundImageUrl,
-      }
-      // wrap tag in BikeTagApiResponse
-      // return BikeTagApiResponse
-
-      console.log("hello, world*!", {tag, tagData})
-
-
-      return (tagData as any) as BikeTagApiResponse<TagData>
-    })
+    // return BikeTagApiResponse
+    return response as BikeTagApiResponse<TagData>
+  })
 }
