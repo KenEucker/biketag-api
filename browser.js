e53e9d5c7828dff3a001ae0790b184a3e4c02ab6
Ken Eucker <keneucker@gmail.com>
2021-11-16 19:06:47 -0800

    Updates to biketag@1.9.1

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-function biketagExample(options) {
+function biketagExample(options = {}) {
     let num_pages = 0;
     let current_page = 1;
     let previous_page = current_page;
@@ -11,7 +11,7 @@ function biketagExample(options) {
             window.biketagApi = new biketag(opts)
         } else {
             console.log('setting new BikeTag config', {
-                config: window.biketagApi.setConfiguration(opts, false, reinitialize)
+                config: window.biketagApi.config(opts, false, reinitialize)
             })
         }
 
@@ -102,7 +102,7 @@ function biketagExample(options) {
                     subreddit: game.data.subreddit,
                 },
             }, true)
-            const config = biketagAPI.getConfiguration()
+            const config = biketagAPI.config()
 
             const albumTagsData = await biketagAPI.getTags(undefined, config.imgur.hash ? {
                 source: 'imgur'
