3e058fdecebcf927cc26f8b8ca5bfdc62bacdcde
Ron S <ron@nonara.com>
2021-12-06 17:42:29 -0500

    refactor: Initial commit

--- test/src/imgur/getTags.test.ts
+++ test/src/imgur/getTags.test.ts
@@ -1,8 +1,13 @@
+import { ImgurClient } from 'imgur'
+
 const imgurGetTagsMethod = 'biketag.images.getTags'
+
 describe(imgurGetTagsMethod, () => {
   test.todo(`${imgurGetTagsMethod} method requires ImgurHash from payload`)
 
+  // Validate `data` field for all three branches of payload conditional
   test.todo(`${imgurGetTagsMethod} method resolves data of type Tag[]`)
 
+  // Check `getTags` result.status === HttpStatusCode.Ok
   test.todo(`${imgurGetTagsMethod} method resolves status of HttpStatusCode.Ok`)
 })
