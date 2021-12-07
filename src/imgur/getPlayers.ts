32d182ff6c927d245f9fa15ccca329904e871189
Ken Eucker <keneucker@gmail.com>
2021-12-07 03:08:07 -0800

    fix(imgur): fixes the setting of both mystery and found images for the Imgur getPlayers method
    now, each player will have two tags per submission.

--- src/imgur/getPlayers.ts
+++ src/imgur/getPlayers.ts
@@ -45,20 +45,23 @@ export async function getPlayers(
     for (const tag of tags) {
       const mysteryPlayerIncludedIndex = playerNames.indexOf(tag.mysteryPlayer)
       const mysteryPlayerNotYetIncluded = mysteryPlayerIncludedIndex === -1
-      const foundPlayerIncludedIndex = playerNames.indexOf(tag.foundPlayer)
-      const foundPlayerNotYetIncluded = foundPlayerIncludedIndex === -1
       const includeMysteryPlayerInList = payload.slugs?.length
         ? payload.slugs.indexOf(tag.mysteryPlayer) !== -1
-        : true
-      const includeFoundPlayerInList = payload.slugs?.length
-        ? payload.slugs.indexOf(tag.foundPlayer) !== -1
         : true
 
       if (includeMysteryPlayerInList && mysteryPlayerNotYetIncluded) {
         includePlayer(tag, tag.mysteryPlayer)
       } else if (includeMysteryPlayerInList && !mysteryPlayerNotYetIncluded) {
         playersData[mysteryPlayerIncludedIndex].tags.push(tag)
-      } else if (includeFoundPlayerInList && foundPlayerNotYetIncluded) {
+      }
+
+      const foundPlayerIncludedIndex = playerNames.indexOf(tag.foundPlayer)
+      const foundPlayerNotYetIncluded = foundPlayerIncludedIndex === -1
+      const includeFoundPlayerInList = payload.slugs?.length
+        ? payload.slugs.indexOf(tag.foundPlayer) !== -1
+        : true
+
+      if (includeFoundPlayerInList && foundPlayerNotYetIncluded) {
         includePlayer(tag, tag.foundPlayer)
       } else if (includeFoundPlayerInList && !foundPlayerNotYetIncluded) {
         playersData[foundPlayerIncludedIndex].tags.push(tag)
