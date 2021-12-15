513014c26e8f7ae3d3a061ec33beddf9550357d4
Ron S <ron@nonara.com>
2021-12-15 17:19:20 -0500

    test(imgur): Completed `getTags` test

---
+++ test/assets/credentials.ts
@@ -0,0 +1,8 @@
+import { ImgurCredentials } from '../../lib/common/types'
+
+export const imgur: ImgurCredentials = {
+  clientId: '47da78be7590eaa',
+  hash: 'Y9PKtpI',
+  clientSecret: <any>undefined,
+  accessToken: <any>undefined,
+}
--- test/src/imgur/getTags.test.ts
+++ test/src/imgur/getTags.test.ts
@@ -1,13 +1,60 @@
 import { ImgurClient } from 'imgur'
+import * as getTagsModule from '../../../src/imgur'
+import * as credentials from '../../assets/credentials'
+import { Tag } from '../../../lib/common/schema'
+import { HttpStatusCode } from '../../../lib/common/enums'
+
+/// ****************************  Config   *************************** ///
 
 const imgurGetTagsMethod = 'biketag.images.getTags'
 
+// TODO - Make sure these are valid
+const tagNumbers = [431, 430]
+const slugs = ['portland-tag-431', 'portland-tag-430']
+const game = 'portland'
+const hash = 'Y9PKtpI'
+
+/// ****************************  Helpers   *************************** ///
+
+// TODO - Could be more complete - should also move this to main src or a shared
+//  helper file in test dir
+/** TypeGuard for Tag */
+const isTag = (v: any): v is Tag =>
+  typeof v.slug === 'string' && typeof v.tagnumber === 'number'
+
+/// ****************************  Tests   *************************** ///
+
 describe(imgurGetTagsMethod, () => {
-  test.todo(`${imgurGetTagsMethod} method requires ImgurHash from payload`)
+  const client = new ImgurClient(credentials.imgur)
+  const getTags = getTagsModule.getTags.bind(undefined, client)
 
-  // Validate `data` field for all three branches of payload conditional
-  test.todo(`${imgurGetTagsMethod} method resolves data of type Tag[]`)
+  test(`${imgurGetTagsMethod} method requires ImgurHash from payload`, () => {
+    return expect(getTags(<any>{})).rejects.toThrow()
+  })
 
-  // Check `getTags` result.status === HttpStatusCode.Ok
-  test.todo(`${imgurGetTagsMethod} method resolves status of HttpStatusCode.Ok`)
+  describe(`${imgurGetTagsMethod} method resolves data of type Tag[]`, () => {
+    test(`Input: tagnumbers`, async () => {
+      const res = await getTags({ tagnumbers: tagNumbers, game, hash })
+
+      expect(Array.isArray(res.data))
+      res.data.forEach((tag) => expect(isTag(tag)).toBeTruthy())
+    })
+
+    test(`Input: slugs`, async () => {
+      const res = await getTags({ slugs, game, tagnumbers: [] })
+
+      expect(Array.isArray(res.data))
+      res.data.forEach((tag) => expect(isTag(tag)).toBeTruthy())
+    })
+  })
+
+  test(`${imgurGetTagsMethod} method resolves status of HttpStatusCode.Ok`, () => {
+    return expect(
+      getTags({
+        tagnumbers: tagNumbers,
+        game,
+        hash,
+      })
+    ).resolves.toMatchObject({ status: HttpStatusCode.Ok })
+  })
 })
