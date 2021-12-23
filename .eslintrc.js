a201f1de3327094eb4bd117fc9b4fffc248bb948
Ron S <ron@nonara.com>
2021-12-23 16:50:43 -0500

    build: config tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-namespace': 'off',
     'no-async-promise-executor': 'off',
     semi: 'off',
   },
