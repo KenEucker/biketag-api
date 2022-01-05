54d372174cbc6636c03c53ca71ccc964b6777445
Ron S <ron@nonara.com>
2022-01-05 17:53:00 -0500

    test(imgur): Added `updateTag` & `uploadTagImage` tests

--- test/tests/imgur/uploadTagImage.test.ts
+++ test/tests/imgur/uploadTagImage.test.ts
@@ -1,16 +1,62 @@
+import { MockImgur, MockTag } from '#test-src'
+import * as imgurModule from '#src/imgur'
+import { isTag, Tag } from '#src/common/schema'
+import { HttpStatusCode } from '#src/common/enums'
+import { BikeTagApiResponse } from '#src/common/types'
+
+/// ***************************  Config  *************************** ///
+
 const imgurUploadTagImageMethod = 'biketag.images.uploadTagImage'
+const img1 = {
+  imageHash: MockTag.hashes[0],
+  title: MockTag.tags[0].name,
+  description: MockTag.tags[0].name,
+}
+const img2 = {
+  imageHash: MockTag.hashes[1],
+  title: MockTag.tags[1].name,
+  description: MockTag.tags[1].name,
+}
+
+/// ***************************  Tests  *************************** ///
+
 describe(imgurUploadTagImageMethod, () => {
-  test.todo(
-    `${imgurUploadTagImageMethod} method requires ImgurHash from payload`
-  )
+  const client = MockImgur.createMockClient()
+  const uploadTagImage = imgurModule.uploadTagImage.bind(undefined, client)
 
-  test.todo(
-    `${imgurUploadTagImageMethod} method requires a valid uploadTagImagePayload`
-  )
+  test(`${imgurUploadTagImageMethod} method requires ImgurHash from payload`, () => {
+    return expect(uploadTagImage(<any>{})).rejects.toThrow()
+  })
 
-  test.todo(`${imgurUploadTagImageMethod} method resolves data of type Tag[]`)
+  test(`${imgurUploadTagImageMethod} method requires a valid uploadTagImagePayload`, () => {
+    // TODO - When code is working, validate that it's throwing the right error
+    return expect(uploadTagImage(<any>{})).rejects.toThrow()
+  })
 
-  test.todo(
-    `${imgurUploadTagImageMethod} method resolves status of HttpStatusCode.Ok`
-  )
+  describe(`${imgurUploadTagImageMethod} method resolves data of type Tag[]`, () => {
+    test(`Single Image`, async () => {
+      const res = (await uploadTagImage(img1)) as BikeTagApiResponse<Tag>
+      expect(isTag(res.data)).toBeTruthy()
+    })
+
+    test(`Multiple Tags`, async () => {
+      const responses = (await uploadTagImage([
+        img1,
+        img2,
+      ])) as BikeTagApiResponse<Tag>[]
+
+      expect(Array.isArray(responses))
+      expect(responses.length).toBe(2)
+
+      // TODO - Might want to improve this by ensuring the actual results are
+      //   correct and/or in the right order. Maybe better as a separate test
+      responses.forEach((res) => expect(isTag(res.data)).toBeTruthy())
+    })
+  })
+
+  test(`${imgurUploadTagImageMethod} method resolves status of HttpStatusCode.Ok`, () => {
+    return expect(uploadTagImage(img1)).resolves.toMatchObject({
+      status: HttpStatusCode.Ok,
+    })
+  })
 })
