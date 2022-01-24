e447d019f5613f6967ba0509eb78c16234b3b82d
Ken Eucker <keneucker@gmail.com>
2022-01-24 09:12:23 -0800

    fix(license): licensed under the GNU Affero GPL

--- src/biketag/helpers.ts
+++ src/biketag/helpers.ts
@@ -2,9 +2,9 @@ import { BIKETAG_API_HOST, API_VERSION } from '../common/endpoints'
 
 export const getApiUrl = (host = '', endpoint = '', subdomain = ''): string =>
   `${
-    host.length
-      ? host
-      : `https://${
-          subdomain.length ? `${subdomain}.` : ''
-        }${BIKETAG_API_HOST}/${API_VERSION}`
-  }/${endpoint}`
+    host.length && host.indexOf('http') === 0
+      ? host // full host
+      : `https://${subdomain.length ? `${subdomain}.` : ''}${
+          host.length ? host : BIKETAG_API_HOST // partial host
+        }/${API_VERSION}` // version
+  }/${endpoint}` // path
