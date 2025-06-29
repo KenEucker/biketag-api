042e80eb42aa7f669b0b34adf31c703043700472
Ken Eucker <keneucker@gmail.com>
2025-06-29 12:34:05 -0700

    test(vitest.config): cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   test: {
     environment: 'node',
     include: ['test/src/**/*.{test,spec}.ts'],
-    globals: true, // if you want describe/it/expect globally
-    deps: {
-      inline: true, // ensures ts dependencies are properly transformed
-    },
-    // threads: true, // run tests in parallel
   },
   resolve: {
     alias: {
